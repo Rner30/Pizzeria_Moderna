c1fd7f7daee3bf03f316d26cf436ff686087a17e
Dario Aguirre <rner0015@gmail.com>
2021-06-23 12:32:14 -0300

    cambiosimg

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,10 +1,11 @@
 import product1 from '../../images/product-1.jpg'
 import product2 from '../../images/product-2.jpg'
 import product3 from '../../images/product-3.jpg'
+import product4 from '../../images/featured2.jpg'
 import sweet1 from '../../images/sweet-1.jpg'
 import sweet2 from '../../images/sweet-2.jpg'
 import sweet3 from '../../images/sweet-3.jpg'
-
+import sweet4 from '../../images/sweet.jpg'
 export const productData = [
     {
         img: product1,
@@ -27,6 +28,14 @@ export const productData = [
         alt: 'Pizza',
         name: 'Pizza Vegana',
         desc: 'Salchicha italiana, tomates, olivas,pesto,',
+        price: '$14.99',
+        button: 'Comprar'
+    },
+    {
+        img: product4,
+        alt: 'Pizza',
+        name: 'Pizza 3 sabores',
+        desc: 'Cheddar, carne, salame, queso francés con champignones',
         price: '$14.99',
         button: 'Comprar'
     },
@@ -54,6 +63,14 @@ export const productData2 = [
         name: 'Brownie crujiente',
         desc: 'Doble capa de brownie con topping de chocolate blanco y nueces',
         price: '$6.99',
+        button: 'Comprar'
+    },
+    {
+        img: sweet4,
+        alt: 'Shake',
+        name: 'Shake',
+        desc: 'Shake de leche con donut bañada y crema',
+        price: '$9.99',
         button: 'Comprar'
     },
 ]
